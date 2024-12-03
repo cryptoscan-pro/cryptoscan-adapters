999a117be3950c141c1fa21a0cbf30d670ab6211
Dan Cryptoscan <dancryptoscan@gmail.com>
2024-12-04 05:14:59 +0700

    feat

--- src/projects/cex-api/index.ts
+++ src/projects/cex-api/index.ts
@@ -1,8 +1,14 @@
-function handler() {
+import { v5 } from "uuid";
+
+export async function handler({ id, ...data }: Record<string, any>) {
+	return {
+		key: v5(id, v5.URL),
+		...data,
+	};
 }
 
 export default {
-	type: "cex-api",
+	type: "prices",
 	provider: {
 		ip: "0.0.0.0",
 		handler,
