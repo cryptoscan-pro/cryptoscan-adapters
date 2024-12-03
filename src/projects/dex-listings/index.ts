f8424597ade9551486e405882acd05e04d15dcca
Dan Cryptoscan <dancryptoscan@gmail.com>
2024-12-04 03:03:01 +0700

    feat

--- src/projects/dex-listings/index.ts
+++ src/projects/dex-listings/index.ts
@@ -1,10 +1,32 @@
-function handler() {
+import { v5 } from "uuid";
+import { BigNumber } from "bignumber.js";
+import { fixJsonString } from "../../utils/fixJsonString";
+
+export async function handler(data: Record<string, any>) {
+	if (!data?.content) {
+		return
+	}
+
+	const jsonStartIndex = data.content.indexOf("{");
+	const jsonEndIndex = data.content.indexOf("}") + 1;
+	const jsonString = data.content.substring(jsonStartIndex, jsonEndIndex);
+	const result = JSON.parse(fixJsonString(jsonString));
+
+	return {
+		...result,
+		key: v5(data.key, v5.URL),
+		price: new BigNumber(result.price).toNumber(),
+		priceUsd: new BigNumber(result.price_usd).toNumber(),
+		amount: new BigNumber(result.amount).toNumber(),
+		type: 'listings',
+		variant: 'dex',
+	}
 }
 
 export default {
 	type: "dex-listings",
 	provider: {
-		ip: "0.0.0.0",
+		ip: "178.20.208.99",
 		handler,
 	},
 }
