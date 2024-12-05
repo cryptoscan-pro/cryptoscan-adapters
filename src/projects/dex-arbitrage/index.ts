999be7307b959653b37ba70dcd732906d8f2caac
Dan Cryptoscan <dancryptoscan@gmail.com>
2024-12-06 06:12:58 +0700

    feat

--- src/projects/dex-arbitrage/index.ts
+++ src/projects/dex-arbitrage/index.ts
@@ -1,17 +1,28 @@
+import BigNumber from "bignumber.js"
+
 export async function handler(data: Record<string, any>) {
+	if (!data?.content) {
+		return
+	}
+
+	const jsonStartIndex = data.content.indexOf("{");
+	const jsonEndIndex = data.content.indexOf("}") + 1;
+	const jsonString = data.content.substring(jsonStartIndex, jsonEndIndex);
+	const result = JSON.parse(fixJsonString(jsonString));
+
 	return {
 		type: "arbitrage",
-		variant: data.variant,
-		symbol: data.symbol,
-		exchangeFrom: data.exchangeFrom,
-    exchangeTo: data.exchangeTo,
-    buyPriceFrom: data.buyPriceFrom,
-    buyPriceTo: data.buyPriceTo,
-    totalBuyUSD: data.totalBuyUSD,
-    totalSellUSD: data.totalSellUSD,
-    network: data.network,
-    spread: data.spread,
-    contract: data.contract
+		variant: result.variant,
+		symbol: result.symbol,
+		exchangeFrom: result.exchangeFrom,
+    exchangeTo: result.exchangeTo,
+    buyPriceFrom: new BigNumber(result.buyPriceFrom).toNumber(),
+    buyPriceTo: new BigNumber(result.buyPriceTo).toNumber(),
+    totalBuyUSD: new BigNumber(result.totalBuyUSD).toNumber(),
+    totalSellUSD: new BigNumber(result.totalSellUSD).toNumber(),
+    network: result.network,
+    spread: new BigNumber(result.spread).toNumber(),
+    contract: result.contract
 	}
 }
 
