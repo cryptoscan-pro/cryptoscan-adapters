a88bda315545ad441e78683dc6d218373cd296ac
Dan Cryptoscan <dancryptoscan@gmail.com>
2024-12-08 01:31:48 +0700

    feat

---
+++ src/projects/arbitrage-rates/index.ts
@@ -0,0 +1,28 @@
+import { v5 } from "uuid";
+import { BigNumber } from "bignumber.js";
+import { fixJsonString } from "../../utils/fixJsonString";
+
+export async function handler(data: Record<string, any>) {
+	if (!data?.content) {
+		return
+	}
+
+	const jsonStartIndex = data.content.indexOf("{");
+	const jsonEndIndex = data.content.indexOf("}") + 1;
+	const jsonString = data.content.substring(jsonStartIndex, jsonEndIndex);
+	const result = JSON.parse(fixJsonString(jsonString));
+
+	return {
+    ...data,
+		key: v5(data.key, v5.URL),
+		type: 'arbitrage-rates',
+	}
+}
+
+export default {
+	type: "arbitrage-rates",
+	provider: {
+		ip: "178.20.208.99",
+		handler,
+	},
+}
