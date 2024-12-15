f98cfa5d10792a3a764e8f2916c4aa42609c0318
Dan Cryptoscan <dancryptoscan@gmail.com>
2024-12-15 16:15:15 +0700

    feat(trending): add trending module with handler for processing data content and generating a unique key

---
+++ src/projects/trending/index.ts
@@ -0,0 +1,33 @@
+import { v5 } from "uuid";
+import { BigNumber } from "bignumber.js";
+import { fixJsonString } from "../../utils/fixJsonString";
+
+export async function handler(data: Record<string, any>) {
+	if (!data?.content) {
+		return
+	}
+
+	const jsonStartIndex = data.content.indexOf("{");
+	const jsonEndIndex = data.content.indexOf("}") + 1;
+	const jsonString = data.content.substring(jsonStartIndex, jsonEndIndex);
+	const result = JSON.parse(fixJsonString(jsonString));
+
+	console.log(result)
+
+	return {
+		key: v5(data.key, v5.URL),
+		symbol: result.symbol,
+		reference: result?.reference,
+        contract: result?.contract,
+        change: new BigNumber(result?.change || 0).toNumber(),
+		type: 'trending',
+	}
+}
+
+export default {
+	type: "trending",
+	provider: {
+		ip: "178.20.208.99",
+		handler,
+	},
+}
