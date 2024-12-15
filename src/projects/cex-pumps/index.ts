fdc29a23c9da306d1deaf445b6f90bd9a8877ece
Dan Cryptoscan <dancryptoscan@gmail.com>
2024-12-15 15:12:47 +0700

    fix(cex-pumps/index.ts): add nullish coalescing to handle potential undefined values for price and volume fields refactor(cex-pumps/index.ts): change type from "cex-pumps" to "pumps" for better clarity and consistency in naming

--- src/projects/cex-pumps/index.ts
+++ src/projects/cex-pumps/index.ts
@@ -15,19 +15,19 @@ export async function handler(data: Record<string, any>) {
 	return {
 		key: v5(data.key, v5.URL),
 		symbol: result.symbol,
-		price: new BigNumber(result.price).toNumber(),
-		volume24h: new BigNumber(result.volume24h).toNumber(),
-		volumeChange24h: result.volumeChange24h,
-	  volume: result.volume,
-		bidsUsd: result.bidsUsd,
-		asksUsd: result.asksUsd,
+		price: new BigNumber(result?.price || 0).toNumber(),
+		volume24h: new BigNumber(result?.volume24h || 0).toNumber(),
+		volumeChange24h: new BigNumber(result?.volumeChange24h).toNumber(),
+		volume: new BigNumber(result?.volume).toNumber(),
+		bidsUsd: new BigNumber(result?.bidsUsd).toNumber(),
+		asksUsd: new BigNumber(result?.asksUsd).toNumber(),
 		type: 'price-activity',
 		variant: 'cex',
 	}
 }
 
 export default {
-	type: "cex-pumps",
+	type: "pumps",
 	provider: {
 		ip: "178.20.208.99",
 		handler,
