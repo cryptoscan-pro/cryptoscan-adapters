d87e990fea2350ce2c2ab52b8d6d40308eff4b23
Dan Cryptoscan <dancryptoscan@gmail.com>
2024-12-23 20:32:38 +0700

    feat(liquidations): add variant property to the handler response for better data representation

--- src/projects/liquidations/index.ts
+++ src/projects/liquidations/index.ts
@@ -18,6 +18,7 @@ export async function handler(data: Record<string, any>) {
 		usd: new BigNumber(result.usd).toNumber(),
 		symbol: result.symbol,
 		type: 'liquidations',
+		variant: result.variant,
 		exchange: result.exchange,
 	}
 }
