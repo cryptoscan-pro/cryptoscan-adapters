08ec5893bdbef36c69b18f1f592239cf2a980cb0
Dan Cryptoscan <dancryptoscan@gmail.com>
2024-12-25 02:51:59 +0700

    feat(cex-announcements): enhance handler function to generate UUID and structure announcement data fix(cex-announcements): update type from "cex-announcements" to "announcements" for consistency in data representation

--- src/projects/cex-announcements/index.ts
+++ src/projects/cex-announcements/index.ts
@@ -1,8 +1,17 @@
-function handler() {
+import { v5 } from "uuid";
+
+function handler({ id, ...data }: Record<string, any>) {
+	return {
+		key: v5(id, v5.URL),
+		symbol: data.symbol || '',
+		exchange: data.exchange || '',
+		text: data.text || '',
+		type: 'announcements',
+	};
 }
 
 export default {
-	type: "cex-announcements",
+	type: "announcements",
 	provider: {
 		ip: "0.0.0.0",
 		handler,
