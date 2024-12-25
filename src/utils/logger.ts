5afad734cb4db7ffc1d6f1b2243e05a37579441e
Dan Cryptoscan <dancryptoscan@gmail.com>
2024-12-25 19:42:46 +0700

    feat(logger.ts): export logger instance to allow usage in other modules

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,7 +1,7 @@
 import winston from 'winston';
 import { WinstonTransport as AxiomTransport } from '@axiomhq/winston';
 
-const logger = winston.createLogger({
+export const logger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
   defaultMeta: { service: 'user-service' },
