9c9fc9fcde97f2829beaa0a2907fe1f3ec0a4152
Dan Cryptoscan <dancryptoscan@gmail.com>
2024-12-26 21:22:44 +0700

    feat(futures-arbitrage): implement handler function to process arbitrage data and return structured result
    - Add BigNumber for precise numerical operations.
    - Include fixJsonString utility to handle JSON formatting.
    - Use UUID v5 for generating unique keys.
    - Parse and transform input data to a structured output format.

--- src/projects/futures-arbitrage/index.ts
+++ src/projects/futures-arbitrage/index.ts
@@ -1,4 +1,34 @@
-function handler() {
+import BigNumber from "bignumber.js"
+import { fixJsonString } from "../../utils/fixJsonString";
+import { v5 } from "uuid";
+
+export async function handler(data: Record<string, any>) {
+	if (!data?.content) {
+		return "error"
+	}
+
+	const jsonStartIndex = data.content.indexOf("{");
+	const jsonEndIndex = data.content.lastIndexOf("}") + 1;
+	const jsonString = data.content.substring(jsonStartIndex, jsonEndIndex);
+	const result = JSON.parse(fixJsonString(jsonString));
+
+	console.log(result)
+
+	return {
+		key: v5(data.key, v5.URL),
+		type: "arbitrage",
+		variant: result.variant.toUpperCase().replace('CEX', 'FUTURES'),
+		symbol: result.symbol,
+		exchangeFrom: result.exchangeFrom,
+		exchangeTo: result.exchangeTo,
+		buyPriceFrom: new BigNumber(result.buyPriceFrom).toNumber(),
+		buyPriceTo: new BigNumber(result.buyPriceTo).toNumber(),
+		totalBuyUSD: new BigNumber(result.totalBuyUSD).toNumber(),
+		totalSellUSD: new BigNumber(result.totalSellUSD).toNumber(),
+		network: result.network,
+		spread: new BigNumber(result.spread).toNumber(),
+		contract: result.contract
+	}
 }
 
 export default {
