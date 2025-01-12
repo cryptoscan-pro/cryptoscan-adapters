a6120d6640d601ceba3cb5bfd59d665b9d89a52a
Dan Cryptoscan <dancryptoscan@gmail.com>
2025-01-13 02:56:29 +0700

    feat(relations): add groupKey and field to handler function for enhanced data handling

--- src/projects/relations/index.ts
+++ src/projects/relations/index.ts
@@ -5,6 +5,8 @@ function handler(data: Record<string, any>) {
         key: v5(data.key, v5.URL),
         from: data.from,
         to: data.to,
+        groupKey: data.groupKey,
+        field: data.field,
         type: 'relations',
     }
 }
